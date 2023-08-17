80413b356ef0fc61fb6d968ddd243e78c04f437b
dell du <18588220928@163>
2023-08-18 04:01:41 +0800

    Update

--- project/SAD/sadkernel.py
+++ project/SAD/sadkernel.py
@@ -152,8 +152,8 @@ class SPADEDecoder(nn.Module):
         self.G_middle_3 = SPADEResnetBlock(2 * ic, 2 * ic, norm_G, label_nc)
         self.G_middle_4 = SPADEResnetBlock(2 * ic, 2 * ic, norm_G, label_nc)
         self.G_middle_5 = SPADEResnetBlock(2 * ic, 2 * ic, norm_G, label_nc)
-        self.up_0 = SPADEResnetBlock(2 * ic, ic, norm_G, label_nc)
-        self.up_1 = SPADEResnetBlock(ic, oc, norm_G, label_nc)
+        self.up_0 = ShortcutSPADEResnetBlock(2 * ic, ic, norm_G, label_nc)
+        self.up_1 = ShortcutSPADEResnetBlock(ic, oc, norm_G, label_nc)
         self.conv_img = nn.Conv2d(oc, 3, 3, padding=1)
         self.up = nn.Upsample(scale_factor=2)
         
@@ -220,6 +220,9 @@ class SPADE(nn.Module):
     
 
 class SPADEResnetBlock(nn.Module):
+    '''
+        self.learned_shortcut = (fin != fout),  False
+    '''    
     def __init__(self, fin, fout, norm_G, label_nc, dilation=1):
         super().__init__()
         # norm_G === 'spadespectralinstance'
@@ -230,36 +233,69 @@ class SPADEResnetBlock(nn.Module):
         # create conv layers
         self.conv_0 = nn.Conv2d(fin, fmiddle, kernel_size=3, padding=dilation, dilation=dilation)
         self.conv_1 = nn.Conv2d(fmiddle, fout, kernel_size=3, padding=dilation, dilation=dilation)
-        if self.learned_shortcut:
-            self.conv_s = nn.Conv2d(fin, fout, kernel_size=1, bias=False)
+
         # apply spectral norm if specified
-        if 'spectral' in norm_G:
+        if 'spectral' in norm_G: # xxxx8888
             self.conv_0 = spectral_norm(self.conv_0)
             self.conv_1 = spectral_norm(self.conv_1)
-            if self.learned_shortcut:
-                self.conv_s = spectral_norm(self.conv_s)
         # define normalization layers
         self.norm_0 = SPADE(fin, label_nc)
         self.norm_1 = SPADE(fmiddle, label_nc)
-        if self.learned_shortcut:
-            self.norm_s = SPADE(fin, label_nc)
 
     def forward(self, x, seg1):
         x_s = self.shortcut(x, seg1)
+
         dx = self.conv_0(self.actvn(self.norm_0(x, seg1)))
         dx = self.conv_1(self.actvn(self.norm_1(dx, seg1)))
         out = x_s + dx
         return out
 
     def shortcut(self, x, seg1):
-        if self.learned_shortcut:
-            x_s = self.conv_s(self.norm_s(x, seg1))
-        else:
-            x_s = x
-        return x_s
+        return x
 
     def actvn(self, x):
         return F.leaky_relu(x, 2e-1)
+
+
+class ShortcutSPADEResnetBlock(nn.Module):
+    '''
+        self.learned_shortcut = (fin != fout),  True
+    '''
+    def __init__(self, fin, fout, norm_G, label_nc, dilation=1):
+        super().__init__()
+        # norm_G === 'spadespectralinstance'
+
+        # Attributes
+        fmiddle = min(fin, fout)
+        # create conv layers
+        self.conv_0 = nn.Conv2d(fin, fmiddle, kernel_size=3, padding=dilation, dilation=dilation)
+        self.conv_1 = nn.Conv2d(fmiddle, fout, kernel_size=3, padding=dilation, dilation=dilation)
+        self.conv_s = nn.Conv2d(fin, fout, kernel_size=1, bias=False)
+
+        # apply spectral norm if specified
+        if 'spectral' in norm_G: # xxxx8888
+            self.conv_0 = spectral_norm(self.conv_0)
+            self.conv_1 = spectral_norm(self.conv_1)
+            self.conv_s = spectral_norm(self.conv_s)
+        # define normalization layers
+        self.norm_0 = SPADE(fin, label_nc)
+        self.norm_1 = SPADE(fmiddle, label_nc)
+        self.norm_s = SPADE(fin, label_nc)
+
+    def forward(self, x, seg1):
+        x_s = self.shortcut(x, seg1)
+
+        dx = self.conv_0(self.actvn(self.norm_0(x, seg1)))
+        dx = self.conv_1(self.actvn(self.norm_1(dx, seg1)))
+        out = x_s + dx
+        return out
+
+    def shortcut(self, x, seg1):
+        return self.conv_s(self.norm_s(x, seg1))
+
+    def actvn(self, x):
+        return F.leaky_relu(x, 2e-1)
+
 
 class ResBlock3d(nn.Module):
     """
